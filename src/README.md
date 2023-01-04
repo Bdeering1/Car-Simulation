4896d19da6ca93743466971a9ec36e96462144ac
Bryn Deering <bdeering@uoguelph.ca>
2023-01-03 23:33:31 -0500

    Added some initial structs

---
+++ src/README.md
@@ -0,0 +1,10 @@
+## Car
+- Engine (struct?)
+  - needs to store rpms
+- Transmission (struct?)
+  - needs to store gear ratios
+- Differential
+- Wheels
+  - may need to store wheel speed
+- Other traits
+  - mass, drag, etc.
---
+++ src/car.rs
@@ -0,0 +1,58 @@
+struct Car {
+    engine: Engine,
+    transmission: Transmission,
+    wheels: Wheels,
+    velocity: (f32, f32),
+    acceleration: (f32, f32),
+    mass: f32,
+    drag_coefficient: f32
+}
+
+impl Car {
+
+}
+
+
+struct Engine {
+
+}
+
+impl Engine {
+
+}
+
+
+struct Transmission {
+
+}
+
+impl Transmission {
+
+}
+
+
+struct Wheels {
+
+}
+
+impl Wheels {
+
+}
+
+
+struct Differential {
+
+}
+
+impl Differential {
+
+}
+
+
+struct Brakes {
+
+}
+
+impl Brakes {
+
+}
