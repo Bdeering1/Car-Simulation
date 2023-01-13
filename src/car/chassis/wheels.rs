a49badc1a02c3882012c880813cc2eb8c5909e14
JukeBro <jidjosh2006@gmail.com>
2023-01-12 21:34:20 -0500

    another attempted fix

--- src/car/chassis/wheels.rs
+++ src/car/chassis/wheels.rs
@@ -28,16 +28,12 @@ impl WheelPair {
     pub fn get_force(&mut self, torque: f64, load: f64, car_velocity: (f64, f64), dt: f64) -> f64 {
         self.slip_ratio = self.get_slip(car_velocity);
         println!("{}", self.slip_ratio);
-        self.traction = self.get_traction(load, self.slip_ratio).abs();
+        self.traction = self.get_traction(load, self.slip_ratio);
 
         let traction_torque = self.traction * self.radius;
         self.calc_wheel_acceleration(traction_torque, torque, 0.0 /* TODO: add brakes */, dt);
 
-        if self.is_drive_wheel {
-            self.traction
-        } else {
-            0.0
-        }
+        f64::min(torque * self.radius, self.traction)
     }
 
     fn get_slip(&self, car_velocity: (f64, f64)) -> f64 {
@@ -56,11 +52,10 @@ impl WheelPair {
         //println!("{}", torque);
     }
 
-    fn get_traction(&self, load: f64, mut slip_ratio: f64) -> f64 { //traction in longitudinal force (N)
-        slip_ratio = slip_ratio.clamp(0.0, 70.0);
-        let traction_force: f64 = f64::min(200.0 * slip_ratio, 1200.0);
+    fn get_traction(&self, load: f64, slip_ratio: f64) -> f64 { //traction in longitudinal force (N)
+        let traction_force: f64 = (200.0 * slip_ratio).clamp(-1200.0, 1200.0);
         
-        traction_force * (load/1000.0)
+        traction_force * (load/1000.0) //load in kN
     }
 }
 
