cc8d5791154794fe3bf478fe89d422963776f92d
Bryn Deering <bdeering@uoguelph.ca>
2023-11-18 16:53:22 -0500

    Revert "simplify slip-traction calc & attempt fix traction"
    This reverts commit d8e754f833d68c5a409c3f1e6750bb7e57aaa8e8.

--- src/car.rs
+++ src/car.rs
@@ -51,7 +51,7 @@ impl Car {
         if self.engine.rpm == self.engine.max_rpm {
             self.drive_force = 0.0
         } else {
-            self.drive_force = self.chassis.get_wheel_force(self.drive_force, self.engine.get_torque(0.1) * self.transmission.get_ratio(), self.velocity, dt).0;
+            self.drive_force = self.chassis.get_wheel_force(self.drive_force, self.engine.get_torque(0.01) * self.transmission.get_ratio(), self.velocity, dt).0;
         }
         self.drive_force *= DRIVE_TRAIN_EFFICIENCY;
         self.drag = self.velocity.0.powf(2.0) * self.drag_coefficient;
@@ -64,7 +64,7 @@ impl Car {
         let drive_wheel_rpm: f64 = match self.chassis.drive_wheels {
             DriveWheels::Front => self.chassis.front_wheels.ang_vel * 9.549296585513721,
             DriveWheels::Rear => self.chassis.rear_wheels.ang_vel * 9.549296585513721,
-            DriveWheels::All => self.chassis.rear_wheels.ang_vel * 9.549296585513721, //TODO: avg velocity between both sets of wheels
+            DriveWheels::All => self.chassis.front_wheels.ang_vel * 9.549296585513721, //TODO: avg velocity between both sets of wheels
         };
 
         self.engine.rpm = ((drive_wheel_rpm /* wheel rev/s */ 
@@ -78,7 +78,7 @@ impl Car {
 
 impl Display for Car {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result { 
-        f.write_str(format!("vel: {:.2} km/h acc: {:.2} m/s^2 drive force: {} N hp: {} drag: {} N rolling res: {} N {} {} {}", self.velocity.0 * 3.6, self.acceleration.0, self.drive_force as i32, self.hp as i32, self.drag as i32, self.rolling_res as i32, self.engine, self.transmission, self.chassis.front_wheels).as_str())?;
+        f.write_str(format!("vel: {:.2} km/h acc: {:.2} m/s^2 drive force: {} N hp: {} drag: {} N rolling res: {} N {} {} {}", self.velocity.0 * 3.6, self.acceleration.0, self.drive_force as i32, self.hp as i32, self.drag as i32, self.rolling_res as i32, self.engine, self.transmission, self.chassis.rear_wheels).as_str())?;
         Ok(())
     }
 }
